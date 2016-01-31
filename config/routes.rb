d80eda28c2724a57b987503a3b9f3a7c3331a217
ginpei <ginpei@gmail.com>
2016-01-31 13:05:15 -0800

    remove unnecessary actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   post 'users/say'
-  get 'misc/say'
+
   get '/auth/:provider/callback', to: 'sessions#callback'
   post '/auth/:provider/callback', to: 'sessions#callback'
   get '/logout', to: 'sessions#destroy', as: :logout
-  resources :users
+
   root 'misc#home'
 end
